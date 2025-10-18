ecb8eac28f8f42eddc561620b21fa8f3256729af
khanathippwth <khanathippwth@gmail.com>
2025-10-18 13:11:39 +0700

    Add files via upload

---
+++ moss/factions/shade/net/kyori/adventure/Adventure.java
@@ -0,0 +1,12 @@
+/*
+ * Decompiled with CFR 0.153-SNAPSHOT (d6f6758-dirty).
+ */
+package moss.factions.shade.net.kyori.adventure;
+
+public final class Adventure {
+    public static final String NAMESPACE = "adventure";
+
+    private Adventure() {
+    }
+}
+
